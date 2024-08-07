d245717f6c354c38215ffb37386f03c0f5e4ecfc
AC <167352447+ac0z@users.noreply.github.com>
2024-08-07 18:00:38 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,13 +4,19 @@ import os
 
 def setup_logger():
     today = datetime.now().strftime('%Y-%m-%d')
-    log_file = f'test_log_{today}.log'
+    logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'Logs')
     
-    # Eğer log dosyası yoksa oluştur, varsa mevcut dosyaya ekleme yap
+    # Create Logs directory if it doesn't exist
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
+    
+    log_file = os.path.join(logs_dir, f'test_log_{today}.log')
+    
+    # Create log file if it doesn't exist, otherwise append to the existing file
     if not os.path.exists(log_file):
         open(log_file, 'w').close()
     
     logging.basicConfig(filename=log_file, level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s',
-                        filemode='a')  # 'a' modu, mevcut dosyaya ekleme yapar
+                        filemode='a')  # 'a' mode appends to the existing file
     return logging.getLogger()
